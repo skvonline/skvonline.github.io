ac96d32d492c7d378a4d46df7aef093a87cc8c46
Dominik R. <125642128+richti03@users.noreply.github.com>
2026-04-24 20:29:53 +0200

    Scope default event image to detail page only

--- src/js/script.js
+++ src/js/script.js
@@ -381,7 +381,7 @@ function getEventImagePath(event) {
     return event.image;
   }
 
-  return './src/img/events/default.png';
+  return '';
 }
 
 function getEventShareButtonMarkup(event) {
@@ -1130,7 +1130,7 @@ async function loadEventDetailContent() {
 
   document.title = `SKV | ${matchingEvent.title || 'Veranstaltungsdetails'}`;
 
-  const detailImagePath = normalizeImagePathForSubpage(matchingEvent.image);
+  const detailImagePath = normalizeImagePathForSubpage(matchingEvent.image) || '../src/img/events/default.png';
   const imageMarkup = detailImagePath
     ? `<img class="event-detail-image" src="${detailImagePath}" alt="${matchingEvent.title || 'Veranstaltung'}" loading="lazy" />`
     : '';
